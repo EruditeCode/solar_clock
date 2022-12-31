bcfb935db62d4a9d3d89ed62142feb4c8beea8ec
EruditeCode <erudite.home@gmail.com>
2022-12-31 15:30:39 +0000

    Simple pygame simulation of earth orbiting the sun.

---
+++ main.py
@@ -0,0 +1,62 @@
+import pygame
+import clock_functions as cf
+
+def main():
+	# Pygame Setup
+	pygame.init()
+	WIDTH, HEIGHT = 1000, 600
+	CENTER = (WIDTH//2, HEIGHT//2)
+	screen = pygame.display.set_mode((WIDTH, HEIGHT))
+	clock = pygame.time.Clock()
+
+	# Creating the background surface for the clock.
+	bg = pygame.Surface((WIDTH, HEIGHT))
+	bg.fill((20, 20, 20))
+
+	# Initial setup for the clock.
+	count = 0
+	year = None
+	days = None
+	break_index = None
+	points = []
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit()
+
+		# Collect info...
+		date_year = 2023
+		
+		# Update orbit points based on collected information.
+		if date_year != year or points == []:
+			year = date_year
+			if year % 4 == 0:
+				days, break_index = 366, 179
+			else:
+				days, break_index = 365, 178
+			points = cf.create_orbit_points(days, break_index)
+
+		# Show the background.
+		screen.blit(bg, (0, 0))
+
+		# Drawing the Earth and the clock hand.
+		angle = (count / days) * 360
+		position_earth = cf.get_position(CENTER, points[count], angle)
+		pygame.draw.circle(screen, (255,255,255), position_earth, 6)
+
+		# Drawing the Sun.
+		pygame.draw.circle(screen, (255,255,255), CENTER, 11)
+
+		if count < days - 1:
+			count += 1
+		else:
+			count = 0
+
+		pygame.display.update()
+		clock.tick(60)
+
+
+if __name__ == "__main__":
+	main()
